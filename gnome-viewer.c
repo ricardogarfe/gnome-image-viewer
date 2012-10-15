f2a0d9895aa7f02a107e4929e61375fe0920fbfc
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-10-15 20:42:43 +0200

    Connect GtkScrolledWindow with GtkImage using gtk_scrolled_window_add_with_viewport and load images using gtk_file_chooser_dialog.

--- gnome-viewer.c
+++ gnome-viewer.c
@@ -1,4 +1,3 @@
-// #include <config.h>
 #include <gtk/gtk.h>
 
 static void on_open_image(GtkButton* button, gpointer user_data)
@@ -35,35 +34,65 @@ static void on_open_image(GtkButton* button, gpointer user_data)
 static GtkWidget* create_window (void)
 {
 	GtkWidget *window;
-    // GtkScrolledWindow * scrolledWindow;
+    GtkScrolledWindow *scrolledWindow;
 	GtkWidget *button;
 	GtkWidget *image;
+    GdkPixbuf *imageBuf;
+    GError *error = NULL;
 	GtkWidget *box;
 
-	/* Set up the UI */
+    int width, height;
+
+	// Window.
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (window), "image-viewer-c");
 
-    /* Scrolled Window to include image */
-    // scrolledWindow = gtk_scrolled_window_new();
+	// Signal to Exit when the window is closed.
+	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
+    gtk_widget_show(window);
+
+    // Container Box.
 	box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
+
+    // Add to the container.
+    gtk_container_add (GTK_CONTAINER (window), box);
+
+    // Scrolled Window to include image.
+    scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow),
+                                 GTK_POLICY_AUTOMATIC,
+                                 GTK_POLICY_AUTOMATIC);
+
+    gtk_widget_show(scrolledWindow);
+
+    // Button.
 	button = gtk_button_new_with_label (("Open image"));
-	// image = gtk_image_new ();
+    gtk_widget_show(button);
+
+    // Image Buffer.
+    imageBuf = gdk_pixbuf_new_from_file ("images/doors.jpg", &error);
+    width = gdk_pixbuf_get_width (imageBuf);
+    height = gdk_pixbuf_get_height (imageBuf);
+
+    // Image.
     image = gtk_image_new_from_file("images/doors.jpg");
 
-	gtk_box_pack_start (GTK_BOX (box), image, TRUE, TRUE, 0);
+    // Default size for the window. 
+    gtk_widget_set_size_request (window, width<500?width:500,
+                               height<500?height:500);
+    gtk_widget_show(image);
+
+    // Use ViewPort to set scrollWidow to image.
+    gtk_scrolled_window_add_with_viewport(scrolledWindow, image);
+
+    // Add Widgets.
+    gtk_box_pack_start (GTK_BOX (box), scrolledWindow, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (box), image, TRUE, TRUE, 0);
 	gtk_box_pack_start (GTK_BOX (box), button, FALSE, FALSE, 0);
 
-	gtk_container_add (GTK_CONTAINER (window), box);
-
-	/* Connect signals */
-
-	/* Show open dialog when opening a file */
-	// g_signal_connect (button, "clicked", G_CALLBACK (on_open_image), image);
-
-	/* Exit when the window is closed */
-	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+	// Show open dialog when opening a file.
+	g_signal_connect (button, "clicked", G_CALLBACK (on_open_image), image);
 
 	return window;
 }
@@ -71,12 +100,12 @@ static GtkWidget* create_window (void)
 int main (int argc, char **argv)
 {
 
+    // Initialize gtk.
     gtk_init(&argc, &argv);
 
     GtkWidget *window;
     window = create_window();
 
-    // gtk_widget_show(GTK_WIDGET (window));
     gtk_widget_show_all(GTK_WIDGET (window));
 
     gtk_main();
